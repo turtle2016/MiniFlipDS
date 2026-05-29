7f1861c0c4106a8d3fd8f6afc9c4684a884b0a16
turtle2016 <Grsanders84@gmail.com>
2026-05-28 21:41:44 -0500

    main.c

--- source/main.c
+++ source/main.c
@@ -1,1 +1,11 @@
 source/main.c
+#include <nds.h>
+
+int main() {
+    videoSetMode(MODE_5_2D);
+    consoleDemoInit();
+
+    iprintf("MiniFlip DS GitHub build working!\n");
+
+    while (1) swiWaitForVBlank();
+}
